2ee9d951da694562e0d986e0a7879bfb8247d6d3
krissdud <newdks@mail.ru>
2022-11-09 21:25:54 +0300

    feat:delete extra code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,14 +1,4 @@
-/*const fs = require('fs');
-const readFile = fs.createReadStream('./01-read-file/text.txt')
-readFile.on('data', chunk => console.log(chunk.toString())); */
-
 const fs = require('fs');
-const path = require('path');
-fs.readFile(
-    path.join(__dirname, 'text.txt'),
-    'utf-8',
-    (err, data) =>{
-        if (err) throw err;
-        console.log(data);
-    }
-);
+const readFile = fs.createReadStream('./01-read-file/text.txt')
+readFile.on('data', chunk => console.log(chunk.toString())); 
+
